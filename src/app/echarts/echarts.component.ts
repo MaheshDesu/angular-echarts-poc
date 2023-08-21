be7e124665f2fc111734e1b058e8d16009c222b9
Mahesh Desu <maheshgurukanthdesu@gmail.com>
2023-08-21 11:34:36 -0400

    Added echarts dependency and component

---
+++ src/app/echarts/echarts.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { EChartsOption } from 'echarts';
+
+@Component({
+  selector: 'app-echarts',
+  templateUrl: './echarts.component.html',
+  styleUrls: ['./echarts.component.scss']
+})
+
+export class EchartsComponent {
+    
+  chartOption: EChartsOption = {
+    xAxis: {
+      type: 'category',
+      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+    },
+    yAxis: {
+      type: 'value',
+    },
+    series: [
+      {
+        data: [820, 932, 901, 934, 1290, 1330, 1320],
+        type: 'line',
+      },
+    ],
+  };
+
+}
